e7e491a2eeb9467a39102224dc5b8d77e1eac74c
Bruce <brucealberto2@gmail.com>
2022-06-10 17:10:58 -0300

    fix lint

--- src/middlewares/authToken.ts
+++ src/middlewares/authToken.ts
@@ -1,13 +1,13 @@
-import jwt from 'jsonwebtoken';
-import { StatusCodes } from 'http-status-codes';
-import { NextFunction, Request, Response } from 'express';
+// import jwt from 'jsonwebtoken';
+// import { StatusCodes } from 'http-status-codes';
+// import { NextFunction, Request, Response } from 'express';
 
-const secretPassword = 'secretPassword';
+// const secretPassword = 'secretPassword';
 
-const authToken = async (req: Request, res: Response, next: NextFunction):Promise<Response> => {
-  const tokenHeader:string = req.headers.authorization;
-  const decoded = jwt.verify(tokenHeader, secretPassword);
+// const authToken = async (req: Request, res: Response, next: NextFunction):Promise<Response> => {
+//   const tokenHeader:string = req.headers.authorization;
+//   const decoded = jwt.verify(tokenHeader, secretPassword);
   
-  next();
-};
-export default authToken;
+//   next();
+// };
+// export default authToken;
